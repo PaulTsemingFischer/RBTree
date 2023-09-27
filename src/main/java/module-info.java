470064daedc3e5e01c56e4487ab45267dc051405
Paul F <paultsemingfischer@gmail.com>
2023-09-26 22:52:01 -0400

    Configuring Kotlin in Maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.github.paultsemingfischer.rbtree {
 
     opens com.github.paultsemingfischer.rbtree to javafx.fxml;
     exports com.github.paultsemingfischer.rbtree;
+    exports com.github.paultsemingfischer.rbtree.graphics;
+    opens com.github.paultsemingfischer.rbtree.graphics to javafx.fxml;
 }
