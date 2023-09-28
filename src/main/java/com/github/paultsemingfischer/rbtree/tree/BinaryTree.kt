7b53ab796fe0eec47dd46a6ec2bb4cea95047d5e
Luke Abruzese <117677296+labruzese@users.noreply.github.com>
2023-09-27 23:28:30 -0400

    Deleted RBTree.kt. Restructured BinaryTree.kt to be more understandable and flexible :)

--- src/main/java/com/github/paultsemingfischer/rbtree/tree/BinaryTree.kt
+++ src/main/java/com/github/paultsemingfischer/rbtree/tree/BinaryTree.kt
@@ -1,24 +1,16 @@
 package com.github.paultsemingfischer.rbtree.tree
-
-abstract class BinaryTree<E : Comparable<E>, N : BinaryTree.Node<E, N>> {
-
-    abstract var rootNode: N?
+abstract class BinaryTree<E : Comparable<E>, N : BinaryTree<E,N>.Node>(var rootNode: N? = null) {
 
     abstract fun addNode(node : N) : N
 
     abstract fun removeNode() : N
 
-    abstract class Node<E : Comparable<E>, N : Node<E, N>>( //N is the specific Node type that you are
-        var data: E,
-        var left: N? = null,
-        var right: N? = null) : Comparable<N> {
-
+    abstract inner class Node(var data: E, var left: N? = null, var right: N? = null) : Comparable<N> {
         open fun isLeaf(): Boolean{
             return (left == null && right == null)
         }
         override fun compareTo(other: N): Int{
             return data.compareTo(other.data)
         }
-        //we can create abstract functions here
     }
 }
