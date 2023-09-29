88495a1b9aa12c5ddb4dd5cb4ac5d77d5e017b86
Luke Abruzese <24labruzese@xbhs.net>
2023-09-29 12:30:27 -0400

    i like generics slightly less after this commit

--- src/main/java/com/github/paultsemingfischer/rbtree/tree/BinarySearchTree.kt
+++ src/main/java/com/github/paultsemingfischer/rbtree/tree/BinarySearchTree.kt
@@ -1,23 +1,55 @@
 package com.github.paultsemingfischer.rbtree.tree
 
 class BinarySearchTree<E : Comparable<E>, N : BinarySearchTree<E,N>.BSTNode>(rootNode: N? = null) : BinaryTree<E, N>(rootNode) {
+    override fun add(node : N) : N {
+        //Handle empty tree
+        if(rootNode == null) {rootNode = node; return node}
 
-    override fun add(node: N): N {
+        //Navigate down tree
+        var next = rootNode
+        var currentNode : N = next!!
+        while (next != null){
+            currentNode = next
+            next = if(node.data >= currentNode.data) currentNode.right else currentNode.left
+        }
+        //some pretty cool syntax I came across
+        //Add node at our determined location
+        currentNode.run {
+            if (node.data >= data) right = node
+            else left = node
+        }
+        return node;
+    }
+
+    override fun remove(value: E): N? {
         TODO("Not yet implemented")
     }
-    override fun remove(): N {
+
+    override fun remove(node: N): N {
         TODO("Not yet implemented")
     }
-    fun findOrNull(value : E) : N? {
-        TODO() //Binary search for value. Return Node or null
+
+    fun findNodeOrNull(value : E) : N? {
+        var currentNode = rootNode
+        while (currentNode != null){
+            if(currentNode.data == value) return currentNode
+            currentNode = if(value >= currentNode.data) currentNode.right else currentNode.left
+        }
+        return null
     }
-    fun exists(value : E) : Boolean {
-        return findOrNull(value) != null
+
+    fun contains(value : E) : Boolean {
+        return findNodeOrNull(value) != null
     }
+
+
     inner class BSTNode(data: E, val parent: N?, left: N? = null, right: N? = null)
         : BinaryTree<E, N>.Node(data, left, right), Comparable<N> {
+
         fun isLeftChild() : Boolean { return parent?.left == this }
+
         fun isRightChild() : Boolean { return parent?.right == this }
+
         override fun compareTo(other: N): Int{
             return data.compareTo(other.data)
         }
--- src/main/java/com/github/paultsemingfischer/rbtree/tree/BinaryTree.kt
+++ src/main/java/com/github/paultsemingfischer/rbtree/tree/BinaryTree.kt
@@ -1,8 +1,20 @@
 package com.github.paultsemingfischer.rbtree.tree
 abstract class BinaryTree<E, N : BinaryTree<E,N>.Node>(var rootNode: N? = null) {
+
     abstract fun add(node : N) : N
-    abstract fun remove() : N
-    abstract inner class Node(var data: E, var left: N? = null, var right: N? = null){
+
+    abstract fun remove(value : E) : N?
+    abstract fun remove(node : N) : N
+    open fun removeAll(value: E): List<N> {
+        val removedNodes: MutableList<N> = mutableListOf()
+        var removedNode: N?
+        while (remove(value).also { removedNode = it } != null) {
+            removedNodes.add(removedNode!!)
+        }
+        return removedNodes
+    }
+
+    abstract inner class Node(var data: E, var left: N?, var right: N?){
         open fun isLeaf(): Boolean{
             return (left == null && right == null)
         }
