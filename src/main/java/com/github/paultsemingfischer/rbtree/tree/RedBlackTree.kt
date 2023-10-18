3e711876537330b1b3d44b2d5f29144b62636688
Luke Abruzese <24labruzese@xbhs.net>
2023-10-18 14:24:10 -0400

    possibly PROBABLY working fixup

--- src/main/java/com/github/paultsemingfischer/rbtree/tree/RedBlackTree.kt
+++ src/main/java/com/github/paultsemingfischer/rbtree/tree/RedBlackTree.kt
@@ -14,29 +14,50 @@ class RedBlackTree<E : Comparable<E>>(inputList: List<E> = emptyList()) : Binary
     }
 
     private fun insertFixUp(node : RBNode<E>){
-        //we can directly access parent if there's no need to access RB specific properties like color
+        //Welcome into the world, you are an unimportant red child, already (probably) causing chaos within the kingdom
         var you = node
-        if(you.parent == null) {you.color = RBNode.RBColor.BLACK; return} //you are root
+        //If you were born from nothing, and you have no parents you are the king of the entire realm
+        //The king is important and thus must have the famous well-known black status
+        if(you.parent == null) {you.color = RBNode.RBColor.BLACK; return}
 
-        while (you.getParent()?.color == RBNode.RBColor.RED){ //While parent is red
+        //If your parent is red then we got a problem because you can't have 2 straight generations go unremembered
+        while (you.getParent()?.color == RBNode.RBColor.RED){
+            //this is your dad
             var dad = you.getParent()!!
+            //this is your dads dad
             val grandpa = dad.getParent()!!
-
+            //this is your uncle; your dads only sibling (hopefully he exists)
             val uncle = if(dad.isRightChild()) grandpa.getRight() else grandpa.getLeft()
-            //If parent is left child
-            if (uncle?.color == RBNode.RBColor.RED) { //case 1: uncle is red if null it will return false which is fine because nodes that don't exist are treated as black
-                uncle.color = RBNode.RBColor.BLACK //set uncle to black
-                dad.color = RBNode.RBColor.BLACK
-                grandpa.color = RBNode.RBColor.RED
-                you = grandpa
-            } else {//case 2+3: uncle is black or null
-                if(dad.isRightChild() xor you.isRightChild()){ //true if you are on the opposite side of your dad
+
+            //is your uncle unimportant?
+            //if your uncle doesn't exist... well then he is not red, and would probably be famous if he existed, so we're pretend he is
+            if (uncle?.color == RBNode.RBColor.RED) {
+                //Now lets make him and your dad both to famous so that you and your grandparent can be an unimportant red
+                //CASE 1
+                uncle.color = RBNode.RBColor.BLACK //force your uncle to be famous
+                dad.color = RBNode.RBColor.BLACK //dad gets famous with him
+                grandpa.color = RBNode.RBColor.RED //force grandpa to red, he has such successful children that he's forgotten about
+                you = grandpa //well now you possess the poor soul of your grandpa
+                //Lets see if you're a problem in your family...
+            }
+            //Alright, so your uncle is famous or dead, we treat them the same
+            else {
+                //If you are a middle child that's not good, we want you to be an edge child
+                if(dad.isRightChild() xor you.isRightChild()){
+                    //CASE 2: Uncle is black or null, you are a middle child
+                    //Switch bodies with your dad
                     you = dad.also{dad = you}
-                    if(dad.isRightChild()) rotateLeft(you) else rotateRight(you) //rotates you the opposite side of your dad
+                    //Now you, in the body of your dad, want to move to the edge (the same side to your dad as your dad is to his dad)
+                    //Formally make yourself a child of your dad again but on the edge
+                    if(you.isRightChild()) rotateLeft(you) else rotateRight(you) //rotates you the edge (same side of your dad)
+                    //Well we didn't quite solve the problem, you and your dad is still both unimportant but now that an easy fix
                 }
-                //case 3: you are left child
+                //CASE 3
+                //make dad famous like his brother
                 dad.color = RBNode.RBColor.BLACK
+                //force grandpa to become red, he has such successful children that he's forgotten about
                 grandpa.color = RBNode.RBColor.RED
+                //you and your grandpa have so much in common you might as well be brothers...
                 if(dad.isRightChild()) rotateRight(grandpa) else rotateLeft(grandpa)
             }
         }
