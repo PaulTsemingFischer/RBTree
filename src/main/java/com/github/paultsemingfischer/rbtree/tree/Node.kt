d65b03402903037a7f6063786216ad96da96fe8e
Paul F <paultsemingfischer@gmail.com>
2023-11-04 20:32:13 -0400

    Removing javafx files

--- src/main/java/com/github/paultsemingfischer/rbtree/tree/Node.kt
+++
@@ -1,7 +0,0 @@
-package com.github.paultsemingfischer.rbtree.tree
-
-interface Node<E, N : Node<E, N>>{
-    var data: E
-    var left: N?
-    var right: N?
-}
--- src/main/java/com/github/paultsemingfischer/rbtree/tree/testing.kt
+++
@@ -1,7 +0,0 @@
-package com.github.paultsemingfischer.rbtree.tree
-
-fun main() {
-    val rbt = RedBlackTree(listOf(8, 3, 2, 0))
-    println("Final tree")
-    rbt.printTree()
-}
